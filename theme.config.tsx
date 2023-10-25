10ef9d4fc8980c1a2e6f860140b34e1aa6606a0c
amesky <loup.quehec@gmail.com>
2023-10-25 22:15:55 +0200

    fix some shit

--- theme.config.tsx
+++ theme.config.tsx
@@ -28,8 +28,10 @@ const config: DocsThemeConfig = {
       (defaultLocale === locale ? asPath : `/${locale}${asPath}`)
     return (
       <>
-        <link rel="icon" type="image/png" href="/icons/192x192.png" />
-        <link rel="icon" type="image/x-icon" href="/favicon.ico" />
+        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+        <link rel="manifest" href="/manifest.webmanifest" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         {/* OPEN GRAPH */}
         <meta property="og:url" content={url} />
