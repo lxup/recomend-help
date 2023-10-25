c7f5d095b8abd7655b60fe98c2eeda4162316058
amesky <loup.quehec@gmail.com>
2023-10-25 17:41:36 +0200

    init

---
+++ config/site.ts
@@ -0,0 +1,34 @@
+export type SiteConfig = typeof siteConfig;
+
+export const siteConfig = {
+  name: 'Support Recomend',
+  description: 'Recomend customer support',
+  url: 'https://support.recomend.app',
+  mainNav: [
+    {
+      title: 'Accueil',
+      href: '/',
+    },
+  ],
+  logo: {
+    href: '/recomend_logo.svg',
+    alt: 'Recomend Logo',
+  },
+  icon: {
+    href: '/recomend_icon.svg',
+    alt: 'Recomend Logo',
+  },
+  links: {
+    discord: 'https://discord.gg/P775UzDJvA',
+    twitter: '@recomend_app',
+    github: 'https://github.com/lxup/recomend-support/',
+    github_repo: 'https://github.com/lxup/recomend-support/tree/main/'
+  },
+  colors: [
+    "#eb4034",
+    "#34cfeb",
+    "#ebc034",
+    "#ad00ab",
+    
+  ]
+};
--- theme.config.tsx
+++ theme.config.tsx
@@ -1,18 +1,102 @@
 import React from 'react'
-import { DocsThemeConfig } from 'nextra-theme-docs'
+import { DocsThemeConfig, LocaleSwitch, ThemeSwitch } from 'nextra-theme-docs'
+import Image from 'next/image'
+import { siteConfig } from './config/site'
+import { useRouter } from 'next/router'
+import Link from 'next/link'
 
 const config: DocsThemeConfig = {
-  logo: <span>My Project</span>,
-  project: {
-    link: 'https://github.com/shuding/nextra-docs-template',
+  useNextSeoProps() {
+    const { asPath } = useRouter()
+    if (asPath !== '/') {
+      return {
+        titleTemplate: `%s – ${siteConfig.name}`
+      }
+    }
+    else {
+      return {
+        titleTemplate: siteConfig.name
+      }
+    }
+  },
+  head:() => {
+    const { asPath, defaultLocale, locale } = useRouter();
+    const url =
+      siteConfig.url +
+      (defaultLocale === locale ? asPath : `/${locale}${asPath}`)
+    return (
+      <>
+        <link rel="icon" type="image/x-icon" href="/favicon.ico"></link>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        {/* OPEN GRAPH */}
+        <meta property="og:url" content={url} />
+        <meta property="og:title" content={siteConfig.name} />
+        <meta property="og:description" content={siteConfig.description}  />
+        {/* <meta property="og:image" content="https://html.sammy-codes.com/images/large-profile.jpg" /> */}
+        {/* TWITTER */}
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta name="twitter:site" content={siteConfig.links.twitter} />
+        <meta name="twitter:title" content={siteConfig.name} />
+        <meta name="twitter:description" content={siteConfig.description} />
+        {/* <meta name="twitter:image" content="https://html.sammy-codes.com/images/large-profile.jpg" /> */}
+      </>
+    )
+  },
+  toc: {
+    backToTop: true,
+  },
+  feedback: {
+    labels: 'Yoo',
+    content: null
+  },
+  editLink: {
+    component: ({children, filePath}) => (
+      <Link href={siteConfig.links.github_repo + filePath}>
+        {children}
+      </Link>
+    )
+  },
+  // project: {
+  //   link: siteConfig.links.github
+  // },
+  logo: 
+    <Image
+      src={siteConfig.logo.href}
+      alt={siteConfig.logo.alt}
+      width={150}
+      height={150}
+      priority
+    />,
+  sidebar: {
+    toggleButton: false
+  },
+  navbar: {
+    extraContent:
+      <>
+        <ThemeSwitch lite/>
+        <LocaleSwitch lite />
+      </>,
   },
   chat: {
-    link: 'https://discord.com',
+    link: siteConfig.links.discord,
   },
-  docsRepositoryBase: 'https://github.com/shuding/nextra-docs-template',
   footer: {
-    text: 'Nextra Docs Template',
+    component: <></>
   },
+  i18n: [
+    { locale: 'en', text: 'English' },
+    { locale: 'fr', text: 'Français' },
+  ],
+  search: {
+    placeholder: () => {
+      const { locale } = useRouter()
+      if (locale === 'fr') {
+        return 'Rechercher dans la docs...'
+      } else {
+        return 'Search inside docs...'
+      }
+    }
+  }
 }
 
 export default config
